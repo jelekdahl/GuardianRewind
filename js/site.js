27c1f995fe7bd9b6cf3f982efed892c2713e3c24
Jon-Erik Ekdahl <jelekdahl@gmail.com>
2023-10-16 13:46:53 -0700

    Completed functionality

--- js/site.js
+++ js/site.js
@@ -1,13 +1,40 @@
-function displayMessage() {
-  //get the text out of our input and assign it to a variable
-  let msg = document.getElementById('message').value;
+// get the user's message to reverse
+function getValues() {
 
-  //alert('Hello Guardian Cohort!');
-  Swal.fire(
-    {
+  // get the text input from the page
+  let message = document.getElementById('userInput').value;
+
+  // validate the input: make sure the input is not empty
+  if (message.length == 0) {
+    Swal.fire({
+      icon: 'error',
       backdrop: false,
-      title: 'APP NAME',
-      text: msg, // <-- replace this text with that variable
-    }
-  );
+      text: 'Please enter a string to reverse'
+    })
+  } else {
+    // pass the input to reverseMessage and assign its return value to a variable
+    let revMsg = reverseMessage(message);
+
+    // give the reversed message to displayMessage to show on the page
+    displayMessage(revMsg);
+  }
+
+}
+
+// reverse the string
+function reverseMessage(input) {
+  // return input.split('').reverse().join(''); // <--- Don't use this in an interview
+  let output = '';
+  
+  for (let i = input.length - 1; i >= 0; i--) {
+    output += input[i];
+  }
+  
+  return output;
+}
+
+// display the reversed message
+function displayMessage(message) {
+  document.getElementById('msg').textContent = `Your message reversed is: ${message}`;
+  document.getElementById('alert').classList.remove('invisible');
 }
